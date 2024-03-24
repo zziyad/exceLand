824108ddcd4bbd45b5487e70ca19cd7ebb19eb15
zziyad <zziyadkhanov@gmail.com>
2024-03-24 15:50:57 +0400

    add functionality for Sign up

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,16 +1,57 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
   const [loading, setLoading] = useState(false);
-  const handleChange = () => { };
-  const handleSubmit = () => { };
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('api/auth/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          id: uuidv4(),
+          type: 'call',
+          method: 'auth/register',
+          args: formData,
+        }),
+      });
+      const {
+        result: { status, responce },
+      } = await res.json();
+      if (status === 'rejected') {
+        setLoading(false);
+        return setErrorMessage(responce);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
   return (
     <div className="min-h-screen mt-20">
       {/* left*/}
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
-        <div className="flex-1">
+        <div className="flex-1 p-4">
           <div>
             <img className="w-70" src="./Exl.png" alt="logo" />
           </div>
@@ -22,9 +63,9 @@ export default function SignUp() {
         </div>
 
         {/* left*/}
-        <div className="flex-1">
+        <div className="flex-1 p-4">
           <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-            <div>
+            <div className="">
               <Label value="Your username" />
               <TextInput
                 type="text"
@@ -56,7 +97,14 @@ export default function SignUp() {
               type="submit"
               disabled={loading}
             >
-              Sign Up
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                'Sign Up'
+              )}
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
@@ -65,6 +113,11 @@ export default function SignUp() {
               Sign In
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
